eee496721555d48a239c65e585ba2c5bd7f5e777
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-26 19:09:39 +0200

    Update dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,2 +1,4 @@
 var headingElement = document.getElementById("main-heading");
-headingElement.innerHTML;
+console.log(headingElement.innerHTML);
+var newHeadingText = prompt("Please provide a new heading:");
+headingElement.innerHTML = newHeadingText;
