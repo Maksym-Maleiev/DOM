2eb55ad2647b81a5ed68b770be20b0736e0cbe7b
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-01 09:09:22 +0200

    Update dom.js

--- js/dom.js
+++ js/dom.js
@@ -26,4 +26,6 @@ for (var i = 0; i < 3; i++){
 
 // Try it
 $("body").append("<p>How are you?</p>");
-$("h1").slideUp(1000).slideDown(1000).fadeOut(2000).fadeIn(2000);
+/* $("h1").slideUp(1000).slideDown(1000).fadeOut(2000).fadeIn(2000); */
+$("h1").fadeOut(2000);
+$("h1").fadeIn(2000);
